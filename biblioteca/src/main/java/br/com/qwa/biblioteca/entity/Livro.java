15b67590bb1bccf769a75f6fb033d61644481a54
qwa-fabioreis <fabio.reis@qwasolucoes.com.br>
2025-07-29 08:51:14 -0300

    feat: criado entidade livro

---
+++ biblioteca/src/main/java/br/com/qwa/biblioteca/entity/Livro.java
@@ -0,0 +1,13 @@
+package biblioteca.src.main.java.br.com.qwa.biblioteca.entity;
+
+import java.time.LocalDate;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private String descricao;
+    private String edicao;
+    private LocalDate anoPublicacao;
+    private boolean disponivel;
+    private LocalDate dataCadastro;
+}
