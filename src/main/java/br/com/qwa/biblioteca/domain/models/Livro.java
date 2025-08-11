2e0c3158828b050d1128781ab69ad89425bb0ff5
qwa-fabioreis <fabio.reis@qwasolucoes.com.br>
2025-08-11 08:25:26 -0300

    feat: mapeado entidade livro

--- src/main/java/br/com/qwa/biblioteca/domain/models/Livro.java
+++ src/main/java/br/com/qwa/biblioteca/domain/models/Livro.java
@@ -1,10 +1,17 @@
 package br.com.qwa.biblioteca.domain.models;
 
 import br.com.qwa.biblioteca.enums.EstadoDoLivroEnum;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.time.LocalDate;
 
+@Table
+@Entity
 public class Livro {
+    @Id
+    private String id;
     private String titulo;
     private String autor;
     private String descricao;
